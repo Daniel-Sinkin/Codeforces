3d01494f81eb573a3d861b47de8d4f1ca0b0e23f
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-03-02 01:22:06 +0100

    Made the setup a bit more fancy

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <iostream>
-
 int main() {
-    std::ios::sync_with_stdio(false);
-    std::cin.tie(nullptr);
-
-    return 0;
+std::ios::sync_with_stdio(false);
+std::cin.tie(nullptr);
+int w;
+std::cin >> w;
+std::cout << ((w & 3) ? "YES" : "NO") << '\n';
 }
